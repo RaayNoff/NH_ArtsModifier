752a22deeeb313220793049a87554addf1ac3da7
RaayNoff <kraxtv471365@gmail.com>
2021-04-19 20:36:34 +0800

    Unused code has been deleted

--- scripts/4_World/Entity/ItemBase/NH_art_Base.c
+++ scripts/4_World/Entity/ItemBase/NH_art_Base.c
@@ -1,27 +1,7 @@
 modded class NH_art_Base extends ItemBase
 {
-	protected Particle 		m_ParticleEfx;
-	protected EffectSound 	m_EngineLoop;
-	protected bool 			m_PhysicsFired;
 	int						m_ModifierID = NHModifiers1.MDF_ART_Modifier;
 	protected PlayerBase	m_ModifiedPlayer;
-
-	void NH_art_Base()
-	{
-		NHDebugPrint("NH_art_Base::NH_art_Base");
-		if ( !GetGame().IsMultiplayer() || GetGame().IsClient() )
-		{
-			GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(SpawnParticle, 100, false);
-			GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(CreateLight, 100, false);
-			GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(IdleSound, 100, false);
-		}
-	}
-
-	void ~NH_art_Base()
-	{
-		NHDebugPrint("NH_art_Base::~NH_art_Base");
-		if(m_EngineLoop) m_EngineLoop.SoundStop();
-	}
 	
 	override void EEItemLocationChanged(notnull InventoryLocation oldLoc, notnull InventoryLocation newLoc)
 	{
@@ -71,79 +51,8 @@ modded class NH_art_Base extends ItemBase
 		}		
 	}
 	
-	void SendModifier(PlayerBase player, bool condition)
-	{
-		if(condition)
-		{
-			player.GetModifiersManager().ActivateModifier(m_ModifierID);
-			return;
-		}
-		
-		player.GetModifiersManager().DeactivateModifier(m_ModifierID);
-	}
-
-	void CreateLight()
-	{
-		NHDebugPrint("NH_art_Base::CreateLight");
-		if(MemoryPointExists("light"))
-		{
-			NHPointLight light = NHPointLight.Cast( ScriptedLightBase.CreateLight( NHPointLight, "0 0 0") );
-			light.SetBrightnessTo(0.3);
-			light.AttachOnMemoryPoint(this, "light");
-			light.SetOrientation("0 0 0");
-			light.SetColorToWhite();
-		}
-	}
-
-	void IdleSound()
-	{
-		NHDebugPrint("NH_art_Base::IdleSound");
-		if(GetIdleSoundSet() != string.Empty)
-		{
-			vector pos = GetPosition();
-			if (!m_EngineLoop)
-			{
-				m_EngineLoop = SEffectManager.CreateSound(GetIdleSoundSet(), pos);
-				m_EngineLoop.SetParent(this);
-				m_EngineLoop.SetSoundWaveKind(WaveKind.WAVEEFFECTEX);
-				m_EngineLoop.SetSoundFadeIn(1);
-				m_EngineLoop.SetSoundFadeOut(1);
-				m_EngineLoop.SetSoundLoop(true);
-				m_EngineLoop.SetSoundVolume(GetIdleSoundVolume());
-				m_EngineLoop.SoundPlay();
-			}
-		}
-	}
-
-	void SpawnParticle()
-	{
-		NHDebugPrint("NH_art_Base::SpawnParticle");
-		if(GetParticleId() != -1)  m_ParticleEfx = Particle.PlayOnObject(GetParticleId(), this);
-	}
-
-	override void EEDelete(EntityAI parent)
-	{
-		NHDebugPrint("NH_art_Base::EEDelete");
-		super.EEDelete(parent);
-		if ( !GetGame().IsMultiplayer() || GetGame().IsClient() )
-		{
-			if ( m_ParticleEfx )
-				m_ParticleEfx.Stop();
-		}
-	}
-
-	float GetIdleSoundVolume()
+	int GetModifierID()
 	{
 		return 1;
-	}
-
-	string GetIdleSoundSet()
-	{
-		return string.Empty;
-	}
-
-	int GetParticleId()
-	{
-		return -1;
 	}
 }
